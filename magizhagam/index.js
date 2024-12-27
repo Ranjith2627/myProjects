c8405c318829a1c4bd05ec307ae6c64668636f0d
Ranjith2627 <ranjithsengeni@gmail.com>
2024-12-27 18:52:12 +0530

    v1

---
+++ magizhagam/index.js
@@ -0,0 +1,177 @@
+let projects = [
+    {
+        "name": "Perambur",
+        "link_name": "perambur",
+        "projectImage": '<img src="assets/img/Pattaravakkam/1.jpg" class="img-fluid" alt="">',
+        "description": `
+			<table class="projectTable">
+				<tbody>
+						<tr><th>Project Name</th><td>Magizhagam</td></tr>
+						<tr><th>Location</th><td>Perambur, Chennai</td></tr>
+						<tr><th>Client</th><td>Mr Thileepan</td></tr>
+						<tr><th>Building Type</th><td>Residential - Ground Floor – 2 BHK –1200 sq ft</td></tr>
+						<tr><th>Status</th><td>Completed & Occupied (February 2024)</td></tr>
+				</tbody>
+			</table>
+			<h5 style="font-weight:bold">About the project :</h5>
+			<p style="text-align:justify;">
+				The completion of the premium residential building marks a new benchmark in luxury living,
+combining modern architectural brilliance with unparalleled comfort. This sophisticated
+structure boasts meticulously crafted interiors, state-of-the-art amenities, and sustainable
+design elements that reflect a commitment to excellence.
+			</p>
+			<p style="text-align:justify;">
+				The project was delivered on schedule, showcasing the expertise and dedication of the
+construction team. This remarkable accomplishment redefines residential standards and is set
+to become a landmark of distinction in the community. The project sits in the middle of Chennai
+city near Perambur Railway station with loads of basic amenities in and around of the house.
+			</p>
+<h5 style="font-weight:bold">Testimonial :</h5>
+			<p style="text-align:justify;font-style:italic;">
+				“Working with Magizhagam Constructions was an outstanding experience from start to finish.
+Their team went above and beyond to bring our vision to life, offering creative solutions and
+meticulous attention to detail at every stage. The project was completed on schedule and within
+budget, a testament to their efficient project management and skilled workforce. Their
+transparency, professionalism, and dedication to quality gave us complete peace of mind. The
+final result exceeded our expectations, and we couldn’t be happier with the outcome.”
+			</p>
+		`,
+        "image": `<div class="swiper-slide"><img src="assets/img/DSC07324.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07332.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07352.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07355.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07418.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07483.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/DSC07519.jpg" class="img-fluid" alt=""></div>`
+    },
+    {
+        "name": "Nemilichery",
+        "link_name": "nemilichery",
+        "projectImage": '<img src="assets/img/Pattaravakkam/1.jpg" class="img-fluid" alt="">',
+        "description": `
+			<table class="projectTable">
+				<tbody>
+						<tr><th>Project Name</th><td>Bala Bhavanam</td></tr>
+						<tr><th>Location</th><td>Nemilichery, Thiruvallur</td></tr>
+						<tr><th>Client</th><td>Mr Kamesh</td></tr>
+						<tr><th>Building Type</th><td>Residential - Ground Floor – 1 BHK – 700 sq ft</td></tr>
+						<tr><th>Status</th><td>Completed & Occupied (August 2024)</td></tr>
+				</tbody>
+			</table>
+			<h5 style="font-weight:bold">About the project :</h5>
+			<p style="text-align:justify;">
+				The structure, designed to combine functionality and aesthetic appeal. Equipped with state-of-
+the-art facilities and built to meet stringent safety and quality standards, the building is now
+ready to welcome its new residents. Its completion not only enhances the surrounding
+community but also provides a comfortable and secure living environment at a serene location
+near Nemilichery Railway station.
+			</p>
+		
+<h5 style="font-weight:bold">Testimonial :</h5>
+			<p style="text-align:justify;font-style:italic;">
+				“We are thrilled with the exceptional service provided by Magizhagam Constructions. From the
+initial planning phase to the final handover, their team demonstrated professionalism,
+expertise, and a commitment to excellence. They not only delivered our project on time but also
+ensured every detail met the highest standards of quality. Communication was seamless
+throughout, with regular updates keeping us informed and involved. Their innovative approach
+and attention to our needs exceeded our expectations, making the entire process stress-free. We
+highly recommend Magizhagam Constructions to anyone seeking reliable and top-notch
+construction services.”
+			</p>
+		`,
+        "image": `<div class="swiper-slide"><img src="assets/img/Nemilichery/1.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/2.png" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/3.png" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/4.png" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/5.png" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/6.png" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/7.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/8.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/9.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Nemilichery/10.jpg" class="img-fluid" alt=""></div>`
+    },
+    {
+        "name": "Pattaravakkam",
+        "link_name": "pattaravakkam",
+        "projectImage": '<img src="assets/img/Pattaravakkam/1.jpg" class="img-fluid" alt="">',
+        "description": `
+<table class="projectTable">
+				<tbody>
+						<tr><th>Project Name</th><td>Poovazhagan’s Residence</td></tr>
+						<tr><th>Location</th><td>Pattaravakkam, Chennai</td></tr>
+						<tr><th>Client</th><td>Mr Poovazhagan</td></tr>
+						<tr><th>Building Type</th><td>Car Park Construction &amp; Residential Building Painting</td></tr>
+						<tr><th>Status</th><td>Completed & Occupied (September 2024)</td></tr>
+				</tbody>
+			</table>
+			<h5 style="font-weight:bold">About the project :</h5>
+			<p style="text-align:justify;">
+				The client wanted a spacious car park to occupy two cars without any hindrance in the parking.
+Also client wanted to add swing, sit out area along with vegetation providing a serene ambience
+also which can be used as a place for family gatherings for recreational purposes. We kept the
+design minimal and ensured the parking area blends well along with his existing residential
+building. Upon completion we also painted the exterior of the building and thereby restoring the
+area to its best possible look.
+			</p>
+		
+<h5 style="font-weight:bold">Testimonial :</h5>
+			<p style="text-align:justify;font-style:italic;">
+				“Thank you guys for your wonderful work Magizhagam Constructions showed great
+professionalism along with in-depth knowledge and the skill was demonstrated throughout the
+project. Customer satisfaction was their priority over money making. Right solutions were
+provided and whatever they promised was delivered at site. Long way to go… keep rocking”
+			</p>
+		`,
+        "image": `<div class="swiper-slide"><img src="assets/img/Pattaravakkam/1.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/2.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/3.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/4.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/5.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/6.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/7.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/8.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/9.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/10.jpg" class="img-fluid" alt=""></div>
+				 <div class="swiper-slide"><img src="assets/img/Pattaravakkam/11.jpg" class="img-fluid" alt=""></div>`
+    }
+]
+
+$(document).ready(() => {
+    let allProjects = "";
+    projects.map((project) => {
+        allProjects += `
+			<div class="col-lg-4 col-md-6 projectLocation" name="${project.link_name}" data-aos="fade-up" data-aos-delay="200">
+				<div class="service-box orange">
+                    ${project.projectImage}
+				  <h3>${project.name}</h3>
+				</div>
+			</div>
+		`;
+    });
+    $(".projectContainer").html(allProjects);
+
+
+    $(".projectLocation").click((e) => {
+        let location = $(e.currentTarget).attr("name");
+
+        $(".modal-title").text("");
+        $(".projectDescription").html("");
+        $(".projectImages").html("");
+
+        projects.map((project) => {
+            if (project.link_name == location) {
+                $(".modal-title").text(project.name);
+                $(".projectDescription").html(project.description);
+                // $(".projectImages").html(project.image);
+
+                $(`#${location}Modal`).modal("show");
+            }
+        });
+
+
+    });
+
+    $(".close").click((e) => {
+        $(".modal").modal("hide");
+    });
+});
